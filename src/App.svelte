92112ce272f3ab8574c13f786afd552cc20a3eeb
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2022-06-29 16:11:50 +0200

    Budget Calculator Setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,18 @@
-<script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
-</script>
+<!-- <script></script> -->
+<!-- JAVASCRIPT LOGIC -->
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Test</h1>
-  <i class="fas fa-home fa-5x"></i>
+<!-- <style></style> -->
+<!-- CSS/STYLING -->
+
+<!-- HTML -->
+<nav class="nav">
+  <div class="nav-center">
+  <h1 class="'nav-title">Budget Calculator</h1>
+    <button type="button" class="nav-btn">
+      <i class="far fa-plus-square" />
+      Add Item
+    </button>
+  </div>
 
 
-  <!-- <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p> -->
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
+</nav>
